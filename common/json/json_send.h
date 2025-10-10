114d5004aa0fa9d9f3f62ca2582aab6104c9d8fb
luozhiyong <luozhiyong131@qq.com>
2025-10-10 14:44:21 +0800

    增加内存监控

--- common/json/json_send.h
+++ common/json/json_send.h
@@ -5,6 +5,22 @@
 #include <QThread>
 #include "tcpsent.h"
 
+#ifdef Q_OS_LINUX
+#include <unistd.h>
+#endif
+
+#define NAME_SIZE  48
+struct sRunTime
+{
+    uint runSec; // 最近开关运行时间 秒钟为单位
+    uint resetCnt; // 重启次数；
+    uint daemonSec; // 守护进程使用
+    char md5[NAME_SIZE]; // 运行程序的ＭＤ５值
+    char start[NAME_SIZE]; // 启动时间
+    char compileTime[NAME_SIZE]; // 编译时间
+    char reserve[3][NAME_SIZE];
+};
+
 class Json_Send : public QThread
 {
     Q_OBJECT
@@ -16,6 +32,20 @@ protected:
     void sendData();
     void TcpsendData();
     void Startbox_Alarm(QJsonObject &obj ,int id);
+
+
+    QString executeCommand(const QString &cmd);
+    int freeMemory();
+    void freeMemoryCheck();
+    void resetProc(sRunTime &proc, const QString &name);
+    void mdelay(int msec);
+    void proc_log(const QString &arg);
+    bool proc_isRun(const QString &p);
+    int isRunning(const char *procname);
+    void proc_start(sRunTime &proc, const QString &app);
+    void proc_time(sRunTime &proc);
+    void proc_md5(sRunTime &proc, const QString &fn);
+    QString md5(const QString &fn);
 private slots:
     void run();
 private:
