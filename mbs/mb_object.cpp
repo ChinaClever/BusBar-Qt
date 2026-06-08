ef093d88399f49760833120c72634180f5b3e5d6
luozhiyong <luozhiyong131@qq.com>
2026-06-08 14:28:24 +0800

    动环协议选配分励脱扣标志

--- mbs/mb_object.cpp
+++ mbs/mb_object.cpp
@@ -114,7 +114,7 @@ void Mb_Object::upSlaveDevInfo(sBusData *data ,int bus, int index)
         vs << dev->version << dev->proNum << dev->loopNum << (index+1) << dev->baudRate;
         vs << dev->iOF << dev->buzzerStatus << dev->alarmTime<<dev->boxType << dev->phaseFlag << dev->plug_cur_spec << dev->backup_breaker;
 
-        vs << 0 << 0 << 0 << 0 ;
+        vs << dev->shuntRelease << 0 << 0 << 0 ;
         sObjData *p = &(dev->data);
         for(int i = 0 ; i < LOOP_NUM_MAX ; i++)
         {
